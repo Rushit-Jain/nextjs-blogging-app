fd1952af050433091b37930f9ff0282c27179192
Rushit Jain <rushit07@gmail.com>
2023-06-11 14:09:44 -0700

    parsed markdown in Post component

---
+++ components/posts/Post.js
@@ -0,0 +1,9 @@
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+function Post(props) {
+  const content =
+    "# This is large heading\n## This is a smaller heading\n### This is even smaller\nThis is for content  \n```this is code```";
+  return <ReactMarkdown>{content}</ReactMarkdown>;
+}
+
+export default Post;
--- pages/[slug].js
+++ pages/[slug].js
@@ -1,9 +1,16 @@
 import { useRouter } from "next/router";
 
+import Post from "../components/posts/Post";
+
 function PostPage(props) {
   const router = useRouter();
   const slug = router.query.slug;
-  return <div>{slug}</div>;
+  return (
+    <>
+      <div>{slug}</div>
+      <Post />
+    </>
+  );
 }
 
 export default PostPage;
