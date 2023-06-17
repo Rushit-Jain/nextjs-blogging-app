12cc65d3854ee6a867f6482df50a96642d97da25
Rushit Jain <rushit07@gmail.com>
2023-06-16 19:30:12 -0700

    completed rendering MD in [slug].js page

--- components/posts/Post.js
+++ components/posts/Post.js
@@ -1,8 +1,39 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import classes from "./Post.module.css";
 
 function Post(props) {
   const post = props.post;
-  return <ReactMarkdown>{post.content}</ReactMarkdown>;
+
+  return (
+    <div className="container">
+      <div className={"row " + classes.card}>
+        <ReactMarkdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  {...props}
+                  children={String(children).replace(/\n$/, "")}
+                  style={atomDark}
+                  language={match[1]}
+                  PreTag="div"
+                />
+              ) : (
+                <code {...props} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {post.content}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
