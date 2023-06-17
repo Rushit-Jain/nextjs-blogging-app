f36c3b935f528edb37935e362cb5ecac0614ca87
Rushit Jain <rushit07@gmail.com>
2023-06-17 08:15:30 -0700

    added title, description, views, upvote count and downvote count in post page

--- components/posts/Post.js
+++ components/posts/Post.js
@@ -2,13 +2,44 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import classes from "./Post.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faThumbsDown,
+  faThumbsUp,
+} from "@fortawesome/free-regular-svg-icons";
 
 function Post(props) {
   const post = props.post;
 
   return (
     <div className="container">
+      <div className="row"></div>
       <div className={"row " + classes.card}>
+        <h1 className={classes.title}>{post.title}</h1>
+        <div className="row">
+          <div className="col-lg-6 col-12">
+            <span>
+              <FontAwesomeIcon icon={faEye} />
+              &nbsp;
+              {post.numberOfViews}
+            </span>
+            &nbsp;&nbsp;
+            <span>
+              <FontAwesomeIcon icon={faThumbsUp} />
+              &nbsp;
+              {post.upvoters.length}
+            </span>
+            &nbsp;&nbsp;
+            <span>
+              <FontAwesomeIcon icon={faThumbsDown} />
+              &nbsp;
+              {post.downvoters.length}
+            </span>
+          </div>
+        </div>
+        <hr />
+        <h4 className={classes.description}>Description: {post.description}</h4>
         <ReactMarkdown
           components={{
             code({ node, inline, className, children, ...props }) {
